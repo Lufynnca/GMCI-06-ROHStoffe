9f0d05d52959431f11a02e7b8d8ad066c0cfb8a9
Lufynnca <luca.eckelmann@stud.uni-hannover.de>
2023-01-09 16:53:10 +0100

    clothesadded

--- gui.js
+++ gui.js
@@ -1,18 +1,19 @@
 
+
 const headDiv = `<img id="detail" src="head_detailed.jpeg" alt="Head" usemap="#workmap">
 <map name="workmap">
     <area class="clothing" id="hat" shape="poly" coords="145,190,150,130,270,110,340,130,360,190" alt="hat" onclick="selectClothing('hat')">
     <area class="clothing" id="hat2" shape="circle" coords="260,85, 40" alt="hat" onclick="selectClothing('hat')">
     <area class="clothing" id="glasses" shape="rect" coords="185,220,310,265" alt="glasses" onclick="selectClothing('glasses')">
-    <area class="clothing" id="earringLeft" shape="rect" coords="120,220,145,310" alt="earringLeft" onclick="selectClothing('earring')">
-    <area class="clothing" id="earringRight" shape="rect" coords="350,220,375,310" alt="earringRight" onclick="selectClothing('earring')">
+    <area class="clothing" id="earringLeft" shape="rect" coords="120,220,145,310" alt="earringLeft" onclick="selectClothing('earrings')">
+    <area class="clothing" id="earringRight" shape="rect" coords="350,220,375,310" alt="earringRight" onclick="selectClothing('earrings')">
 </map>`;
 const  bodyDiv = `<img id="detail" src="hoody_detailed.jpeg" alt="body" usemap="#workmap">
 <map name="workmap">
     <area class="clothing" id="scarf" shape="poly" coords="210,45, 200,230, 260,230, 240,80, 280,80, 280,45" alt="scarf" onclick="selectClothing('scarf')">
     <area class="clothing" id="hoody" shape="poly" coords="120,350, 140,100, 240,50, 300,70, 370,100, 370,320" alt="hoody" onclick="selectClothing('hoody')">
-    <area class="clothing" id="gloveLeft" shape="rect" coords="140,360 ,180,410" alt="gloveLeft" onclick="selectClothing('glove')">
-    <area class="clothing" id="gloveRight" shape="rect" coords="310,360 ,360,410" alt="gloveRight" onclick="selectClothing('glove')">
+    <area class="clothing" id="gloveLeft" shape="rect" coords="140,360 ,180,410" alt="gloveLeft" onclick="selectClothing('gloves')">
+    <area class="clothing" id="gloveRight" shape="rect" coords="310,360 ,360,410" alt="gloveRight" onclick="selectClothing('gloves')">
 </map>
 <button class="arrowRIGHT" onclick="changeBodyOutfit('summer')"></button>
 `
@@ -20,8 +21,8 @@ const  tshirtDiv = `<img id="detail" src="tshirt_detailed.jpeg" alt="body" usema
 <map name="workmap">
     <area class="clothing" id="scarf" shape="poly" coords="210,45, 200,230, 260,230, 240,80, 280,80, 280,45" alt="scarf" onclick="selectClothing('scarf')">
     <area class="clothing" id="hoody" shape="poly" coords="120,350, 140,100, 240,50, 300,70, 370,100, 370,320" alt="hoody" onclick="selectClothing('tshirt')">
-    <area class="clothing" id="gloveLeft" shape="rect" coords="140,360 ,180,410" alt="gloveLeft" onclick="selectClothing('glove')">
-    <area class="clothing" id="gloveRight" shape="rect" coords="310,360 ,360,410" alt="gloveRight" onclick="selectClothing('glove')">
+    <area class="clothing" id="gloveLeft" shape="rect" coords="140,360 ,180,410" alt="gloveLeft" onclick="selectClothing('gloves')">
+    <area class="clothing" id="gloveRight" shape="rect" coords="310,360 ,360,410" alt="gloveRight" onclick="selectClothing('gloves')">
 </map>
 <button class="arrowLEFT" onclick="changeBodyOutfit('winter')"></button>
 `
@@ -38,6 +39,29 @@ const shortsDiv = `<img id="detail"  src="shorts_detailed.jpeg" alt="Legs" usema
 <button class="arrowLEFT" onclick="changeLegsOutfit('winter')"></button>
 `
 
+const clothes={
+    "hat": [
+        { "image" :"pictures/beanie1.png", "price" : "22,99" },{ "image" :"pictures/beanie2.png", "price" : "12,99" },{ "image" :"pictures/beanie3.png", "price" : "29,99" },{ "image" :"pictures/beanie4.png", "price" : "9,99" }
+    ],
+    "glasses": [
+        {"image" :"pictures/googles1.png", "price" : "42,99" },{ "image" :"pictures/googles2.png", "price" : "200,99" },{ "image" :"pictures/googles3.png", "price" : "5,99" },{ "image" :"pictures/googles4.png", "price" : "2,99" }
+    ],
+    "earrings": [
+        {"image":"pictures/earring1.png","price":"70"},{"image":"pictures/earring2.png","price":"420,11"},{"image":"pictures/earring3.png","price":"4,5"},{"image":"pictures/earring4.png","price":"12,99"}
+    ],
+    "scarf":[
+        {"image":"pictures/scarf1.png","price":"7"},{"image":"pictures/scarf2.png","price":"20,31"},{"image":"pictures/scarf3.png","price":"1200,99"},{"image":"pictures/scarf4.png","price":"13,99"}
+    ],
+    "hoody":[
+        {"image":"pictures/hoody1.png","price":"730"},{"image":"pictures/hoody2.png","price":"10,11"},{"image":"pictures/hoody3.png","price":"12,5"},{"image":"pictures/hoody4.png","price":"49,99"}
+    ],
+    "gloves":[
+        {"image":"pictures/glove1.png","price":"12"},{"image":"pictures/glove2.png","price":"14,99"},{"image":"pictures/glove3.png","price":"120,99"},{"image":"pictures/glove4.png","price":"13,99"}
+    ],
+    "tshirt":[
+        {"image":"pictures/tshirt1.png","price":"32"},{"image":"pictures/tshirt2.png","price":"21,99"},{"image":"pictures/tshirt3.png","price":"52,99"},{"image":"pictures/tshirt4.png","price":"42,99"}
+    ]
+    }
 function changeBodyOutfit(season){
     if("summer" == season){
         document.getElementById("detail").innerHTML = tshirtDiv
@@ -54,9 +78,11 @@ function changeLegsOutfit(season){
         document.getElementById("detail").innerHTML = legsDiv
     }
 }
+
 function selectBodyPart(part){
     var allowed = ["head", "body", "legs"]
     //reset
+    document.getElementById("products").innerHTML = ""
     allowed.forEach(function(elem){
         document.getElementById(elem).style.outlineStyle = "none";
         document.getElementById(elem).style.opacity = "1";
@@ -88,7 +114,19 @@ function selectBodyPart(part){
 
 function selectClothing(clothing){
     console.log(clothing);
-    var allowed = ["hat", "glasses", "earring", "scarf", "hoody", "glove"]
+    var allowed = ["hat", "glasses", "earrings", "scarf", "hoody", "gloves", "tshirt"]
+    if(allowed.includes(clothing)){
+        document.getElementById("products").innerHTML = ""
+        //inject
+        clothes[clothing].forEach(function(elem){
+                document.getElementById("products").innerHTML += `
+                <div class="productContainer">
+                 <img class="clothes" src="${elem.image}" alt="${clothing}">
+                 <p>${clothing}         ${elem.price}$</p>
+                </div>`
+            })
+    }
 }
+
 
 
